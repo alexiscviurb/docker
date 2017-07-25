eaf2cc032c0ecee3217eddd887fbe5c3280768ac
Alexis Cvirkun Urbanskyy <alexis.urbanskyy@ihealthgroup.com.br>
2017-07-25 00:25:37 -0300

    Criado repositório gitup-ssh

---
+++ gitup-ssh/up.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+[ "$UP_REPOS" ] || { echo "Nenhum repositório informado!" ; exit 1; }
+[ "$UP_HOST" ] || { echo "Nenhum host informado!" ; exit 1; }
+[ "$UP_USER" ] || { echo "Nenhum usuário informado!" ; exit 1; }
+[ "$UP_FOLDER" ] || { echo "Nenhuma pasta informado!" ; exit 1; }
+
+[ "$UP_FREQUENCY" ] || UP_FREQUENCY=7200
+
+
+if [ ! -f /root/.ssh/id_rsa ]; then
+	# Gera chave 
+	ssh-keygen -t rsa -b 4096 -N "" -f /root/.ssh/id_rsa
+	# SSH yes
+	ssh-keyscan $UP_HOST > /root/.ssh/known_hosts
+fi
+
+gitUp () {
+	repo="$1.git"
+	ssh $UP_USER@$UP_HOST -o PasswordAuthentication=no "cd $UP_FOLDER/$repo/ && git fetch origin +refs/heads/*:refs/heads/* && git reset --soft"
+	if [ $? -gt 0 ]; then
+                echo "Falha ao executar ssh! Pode ser necessário copiar a chave ssh para o servidor de destino!"
+                echo "COMANDO: ssh-copy-id -i /root/.ssh/id_rsa.pub $UP_USER@$UP_HOST"
+        fi
+}
+
+while true; do
+	for REPO in $UP_REPOS; do
+		gitUp $REPO
+	done
+	sleep $UP_FREQUENCY
+done
